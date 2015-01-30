edc98bf283ffcccac1abb28c61a6946d62ddb644
Jon Hurst <jhurst.1024+git@gmail.com>
2015-01-30 17:03:23 +0000

    Added simple shell script to use PrinceXML to create a PDF ebook suitable for either 5 inch diagonal or 6 inch diagonal (i.e. Kindle) ereaders

---
+++ make-ebook.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+DEFAULTCSS_LOCATION="/home/jon/proj/ebook/include"
+
+#Need 2 additional cl arguments, the 2nd of which must be a readable file and
+#the first of which must be '5' or '6'
+if [ $# -ne 2 -o ! -r "${2}" -o "$1" != "5" -a "$1" != "6" ]
+then
+    echo "Usage: $0 [5|6] HTMLFILE"
+    exit -1
+fi
+
+if [ "$1" == "5" ]
+then
+    DEFAULTCSS="${DEFAULTCSS_LOCATION}/prince-5in.css"
+else
+    DEFAULTCSS="${DEFAULTCSS_LOCATION}/prince-6in.css"
+fi
+
+OUTPUTFILE="${2%.*}.pdf"
+
+prince -s $DEFAULTCSS -o $OUTPUTFILE $2
