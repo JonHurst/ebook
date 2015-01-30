d2e068afca5c33a5d0fcd0907b07e66b223a5513
Jon Hurst <jhurst.1024+git@gmail.com>
2015-01-30 17:02:09 +0000

    Add simple tool to replace a tag.class combo with a different tag.class combo

---
+++ replace_tag.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+
+import sys
+import xml.etree.ElementTree as ET
+import argparse
+import os
+import shutil
+
+
+def main():
+    #parse arguments
+    parser = argparse.ArgumentParser(description="""\
+    Converts a tag with optional class to a different tag with a different optional class.
+    Original file is saved with extension .old.""")
+    parser.add_argument("from",
+                        help="tagname or tagname.class to convert from (e.g. 'div' or 'div.para').")
+    parser.add_argument("to",
+                        help="tagname or tagname.class to convert to (e.g. 'p' or 'p.noindent'.")
+    parser.add_argument("input",
+                        help="Input file (xhtml format)")
+    args = vars(parser.parse_args())
+    if not os.path.isfile(args["input"]):
+        print("Error:", args["input"], "is not a file")
+        sys.exit(-1)
+    ET.register_namespace('', "http://www.w3.org/1999/xhtml")
+    shutil.copyfile(args["input"], args["input"] + ".old")
+    i = args["from"].split(".")
+    o = args["to"].split(".")
+    itree = ET.parse(args["input"])
+    if len(i) == 2:
+        xpath = ".//{http://www.w3.org/1999/xhtml}%s[@class='%s']" % tuple(i)
+    else:
+        xpath = ".//{http://www.w3.org/1999/xhtml}%s" % i[0]
+    for e in itree.findall(xpath):
+        e.tag = "{http://www.w3.org/1999/xhtml}" + o[0]
+        del e.attrib["class"]
+        if len(o) == 2:
+            e.set("class", o[1])
+    itree.write(args["input"],
+            encoding="unicode",
+            xml_declaration=True)
+
+
+if __name__ == "__main__": main()
