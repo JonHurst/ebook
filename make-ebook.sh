f698052272f64b7c1058f03face1e3e4ae872d33
Jon Hurst <jhurst.1024+git@gmail.com>
2015-02-03 20:31:53 +0000

    Allow multiple files

--- make-ebook.sh
+++ make-ebook.sh
@@ -4,7 +4,7 @@ DEFAULTCSS_LOCATION="/home/jon/proj/ebook/include"
 
 #Need 2 additional cl arguments, the 2nd of which must be a readable file and
 #the first of which must be '5' or '6'
-if [ $# -ne 2 -o ! -r "${2}" -o "$1" != "5" -a "$1" != "6" ]
+if [ $# -lt 2 -o "$1" != "5" -a "$1" != "6" ]
 then
     echo "Usage: $0 [5|6] HTMLFILE"
     exit -1
@@ -18,5 +18,5 @@ else
 fi
 
 OUTPUTFILE="${2%.*}.pdf"
-
-prince -s $DEFAULTCSS -o $OUTPUTFILE $2
+shift
+prince -s $DEFAULTCSS -o $OUTPUTFILE "$@"
