64f34dbf9fb5b1d9803b8b545206f8488b0c9656
Jon Hurst <jhurst.1024+git@gmail.com>
2015-02-03 08:36:37 +0000

    Replaced decruft.py with dotidy.sh - decruft.py relied on unstable PG semantics and often failed to do more than run tidy anyway.

---
+++ dotidy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ -e "${1}.old" ]
+then
+    echo "${1}.old exists"
+    ANS=""
+    until [[ "$ANS" == "y" || "$ANS" == "n" ]]
+    do read -p "overwrite? [y/n] " ANS
+    done
+    if [[ "$ANS" == "n" ]]
+    then
+        exit -1
+    fi
+fi
+
+cp "${1}" "${1}.old"
+tidy -asxhtml -utf8 --indent auto --quote-nbsp no --write-back yes "$1"
