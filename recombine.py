dab86cadb38ab2f75b362125bce9a760b779b48c
Jon Hurst <jhurst.1024+git@gmail.com>
2015-02-14 09:50:11 +0000

    Improve command line interface

--- recombine.py
+++ recombine.py
@@ -2,7 +2,7 @@
 
 import xml.etree.ElementTree as ET
 import sys
-
+import argparse
 
 title_page_template = """\
 <div id="title_page" xmlns="http://www.w3.org/1999/xhtml">
@@ -38,10 +38,26 @@ def process_bsps(r, bsp_dict):
     return out
 
 
+def parse_command_line():
+    #parse arguments
+    parser = argparse.ArgumentParser(description="""\
+Recombine skeleton and bsps and optionally add title page, creating final xhtml version.
+The title page is generated from the title tag and the author meta tag of the skeleton.""")
+    parser.add_argument("-s", "--skeleton", default="skeleton.xhtml",
+                        help="Name of 'skeleton' input file (default: 'skeleton.xhtml')")
+    parser.add_argument("-b", "--bsps", default="bsps.xhtml",
+                        help="Name of 'bog standard paragraphs' input file (default: 'bsps.xhtml')")
+    parser.add_argument("-o", "--output", default="recombined.xhtml",
+                        help="Output file name (default: 'recombined.xhtml')")
+    parser.add_argument("--notitle", action="store_true",
+                        help="Do not insert title page.")
+    return vars(parser.parse_args())
+
 
 def main():
     ET.register_namespace('', "http://www.w3.org/1999/xhtml")
-    main_tree = ET.parse(sys.argv[1])
+    args = parse_command_line()
+    main_tree = ET.parse(args["skeleton"])
     removeids = main_tree.findall(
         "./{http://www.w3.org/1999/xhtml}head/"
         "{http://www.w3.org/1999/xhtml}link[@href='include/skel_styles.css']")
@@ -50,19 +66,20 @@ def main():
     head = main_tree.find("./{http://www.w3.org/1999/xhtml}head")
     for r in removeids:
         head.remove(r)
-    title = head.find("{http://www.w3.org/1999/xhtml}title").text
-    author_tag = (head.find("{http://www.w3.org/1999/xhtml}meta[@name='author']") or
-                  head.find("{http://www.w3.org/1999/xhtml}meta[@name='author']"))
-    author = author_tag.attrib.get("content")
-    title_page = ET.XML(title_page_template % (title, author))
-    body = main_tree.find("./{http://www.w3.org/1999/xhtml}body")
-    body.insert(0, title_page)
-    bsp_tree = ET.parse(sys.argv[2])
+    if not args.get("notitle"):
+        title = head.find("{http://www.w3.org/1999/xhtml}title").text
+        author_tag = (head.find("{http://www.w3.org/1999/xhtml}meta[@name='author']") or
+                      head.find("{http://www.w3.org/1999/xhtml}meta[@name='author']"))
+        author = author_tag.attrib.get("content")
+        title_page = ET.XML(title_page_template % (title, author))
+        body = main_tree.find("./{http://www.w3.org/1999/xhtml}body")
+        body.insert(0, title_page)
+    bsp_tree = ET.parse(args["bsps"])
     bsp_dict = {}
     for e in bsp_tree.iter("{http://www.w3.org/1999/xhtml}div"):
         bsp_dict[e.attrib["id"]] = e.find("{http://www.w3.org/1999/xhtml}p")
     out = process_bsps(main_tree.getroot(), bsp_dict)
-    ET.ElementTree(out).write("recombined.xhtml",
+    ET.ElementTree(out).write(args["output"],
                               encoding="unicode",
                               xml_declaration=True)
 
